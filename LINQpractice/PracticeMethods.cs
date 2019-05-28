612e1424701f69e1fc73b305375789a2a6e7adf7
Brett Johnson <br.onson@outlook.com>
2019-05-28 10:44:47 -0500

    init

---
+++ LINQpractice/PracticeMethods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQpractice
+{
+    public class PracticeMethods
+    {
+
+        public IEnumerable<string> FindThWords(List<string> words)
+        {
+            var thWords = words.Where(w => w.Contains("th"));
+            return thWords;
+        }
+        public IEnumerable<String> EliminateDoubles(List<string> words)
+        {
+            var singleWords = words.Distinct();
+            return singleWords;
+        }
+        public double GradeAverages(List<string> grades)
+        {
+            var listofAverages = new List<double>();
+            foreach (string originalGrades in grades)
+            {
+                var newGrades = originalGrades.Split(',').Select(s => int.Parse(s)).ToList();
+                var lowestRemoved = newGrades.FindAll(g => g != newGrades.Min());
+                double individualAverage = lowestRemoved.Average();
+                listofAverages.Add(individualAverage);
+            }
+            double finalAverage = listofAverages.Average();
+            return finalAverage;
+        }
+    }
+}
---
+++ LINQpractice/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQpractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PracticeMethods pm = new PracticeMethods();
+            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            var thWords = pm.FindThWords(words);
+            foreach (var word in thWords)
+            {
+                Console.WriteLine(word);
+            }
+            Console.ReadLine();
+
+            List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+            var singleNames = pm.EliminateDoubles(names);
+            foreach (var name in singleNames)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
+
+            List<string> classGrades = new List<string>()
+            {
+            "80,100,92,89,65",
+            "93,81,78,84,69",
+            "73,88,83,99,64",
+            "98,100,66,74,55"
+            };
+            var average = pm.GradeAverages(classGrades);
+            Console.WriteLine(average);
+            Console.ReadLine();
+        }
+    }
+}
